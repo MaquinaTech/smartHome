6af80cbcc4686723886cb03a0ec6e6168aa2c5dc
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-15 17:12:57 +0200

    create model

--- app/Models/Light.php
+++ app/Models/Light.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Group;
+
 
 class Light extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'status', 'description', 'group_id', 'created_at', 'updated_at',
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
+    ];
+
+    /**
+     * Get the group that owns the Light
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
 }
