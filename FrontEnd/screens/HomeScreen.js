e1bdccdbdd54db0200cefd9360406337ea39d1f5
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-15 18:34:35 +0200

    style home screen

--- FrontEnd/screens/HomeScreen.js
+++ FrontEnd/screens/HomeScreen.js
@@ -35,60 +35,103 @@ const ErrorText = styled.Text`
   position: relative;
 `;
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2f2f2;
+`;
+
 const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: blue;
+  color: #333333;
+  margin-bottom: 16px;
 `;
 
 const SensorList = styled.View`
-  /* Estilos adicionales para el contenedor de sensores */
+  margin-top: 32px;
 `;
 
 const SensorItem = styled.View`
-  /* Estilos adicionales para cada sensor */
+  background-color: white;
+  border-radius: 8px;
+  padding: 16px;
+  margin-bottom: 16px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 const SensorName = styled.Text`
-  /* Estilos adicionales para el nombre del sensor */
+  font-size: 18px;
+  font-weight: bold;
+  color: #333333;
 `;
 
 const SensorValue = styled.Text`
-  /* Estilos adicionales para el valor del sensor */
+  font-size: 14px;
+  color: #666666;
 `;
+
+const SensorAC = styled.Text`
+  font-size: 14px;
+  color: lightblue;
+`;
+
+const On = styled.Text`
+  font-size: 14px;
+  color: lightgreen;
+`;
+
+const Off = styled.Text`
+  font-size: 14px;
+  color: red;
+`;
+
+const Group = styled.View`
+  display: flex;
+  flex-direction: row; 
+  align-items: center; 
+  justify-content: flex-start;
+`;
+
 
 const HomeScreen = () => {
   return (
     <SectionContent>
       <SectionContentView>
         <FormView>
-          <Title>Welcome to Home</Title>
+          <Container>
+            <Title>Welcome to Home</Title>
 
-          <SensorList>
+            <SensorList>
+              <SensorItem>
+                <SensorName>Temperatura General</SensorName>
+                <SensorValue>33°C</SensorValue>
+              </SensorItem>
 
-            <SensorItem>
-              <SensorName>Numero de personas</SensorName>
-              <SensorValue>4 Personas</SensorValue>
-            </SensorItem>
+              <SensorItem>
+                <SensorName>Humedad General</SensorName>
+                <SensorValue>50%</SensorValue>
+              </SensorItem>
 
-            <SensorItem>
-              <SensorName>Temperatura general</SensorName>
-              <SensorValue>25°C</SensorValue>
-            </SensorItem>
+              <SensorItem>
+                <SensorName>Personas en la casa</SensorName>
+                <SensorValue>4</SensorValue>
+              </SensorItem>
 
-            <SensorItem>
-              <SensorName>Humedad general</SensorName>
-              <SensorValue>50%</SensorValue>
-            </SensorItem>
-
-            <SensorItem>
-              <SensorName>Aires Acondicionados:</SensorName>
-              <SensorValue>Aire salón: Apagado</SensorValue>
-              <SensorValue>Aire alvaro: Apagado</SensorValue>
-            </SensorItem>
-
-            {/* ... Agrega más elementos de sensor si es necesario */}
-          </SensorList>
+              <SensorItem>
+                <SensorName>Aires Acondicionados</SensorName>
+                <Group>
+                  <SensorAC>AC Salón:</SensorAC>
+                    <On> ON</On>
+                </Group>
+                <Group>
+                  <SensorAC>AC Alvaro:</SensorAC>
+                    <Off> OFF</Off>
+                </Group>
+              </SensorItem>
+            </SensorList>
+          </Container>
         </FormView>
       </SectionContentView>
     </SectionContent>
