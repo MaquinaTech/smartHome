b3e6032d8129a6521940428b7f09ea06dfbc953d
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-15 17:42:09 +0200

    change name

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import MainMenu from './navigation/TabNavigator';
+import TabNavigator from './navigation/TabNavigator';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MainMenu />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
