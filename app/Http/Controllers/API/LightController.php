1bca9b081c61ac945f7f47bf019b1c30b05a7845
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-15 16:09:30 +0200

    move controller to API Folder

---
+++ app/Http/Controllers/API/LightController.php
@@ -0,0 +1,105 @@
+<?php
+       
+namespace App\Http\Controllers\API;
+       
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Light;
+use Validator;
+use App\Http\Resources\LightResource;
+use Illuminate\Http\JsonResponse;
+       
+class LightController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): JsonResponse
+    {
+        $lights = Light::all();
+        
+        return $this->sendResponse(LightResource::collection($lights), 'Lights retrieved successfully.');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $input = $request->all();
+       
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+       
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+       
+        $light = Light::create($input);
+       
+        return $this->sendResponse(new LightResource($light), 'Light created successfully.');
+    } 
+     
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id): JsonResponse
+    {
+        $light = Light::find($id);
+      
+        if (is_null($light)) {
+            return $this->sendError('Light not found.');
+        }
+       
+        return $this->sendResponse(new LightResource($light), 'Light retrieved successfully.');
+    }
+      
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Light $light): JsonResponse
+    {
+        $input = $request->all();
+       
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+       
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+       
+        $light->name = $input['name'];
+        $light->detail = $input['detail'];
+        $light->save();
+       
+        return $this->sendResponse(new LightResource($light), 'Light updated successfully.');
+    }
+     
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Light $light): JsonResponse
+    {
+        $light->delete();
+       
+        return $this->sendResponse([], 'Light deleted successfully.');
+    }
+}
